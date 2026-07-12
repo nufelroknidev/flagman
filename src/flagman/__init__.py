79b5c54aff6b2588524150f96812cd9711825f32
Nufel Rokni <nufel.rokni.dev@gmail.com>
2026-07-12 09:47:42 +0100

    chore: initial project setup

---
+++ src/flagman/__init__.py
@@ -0,0 +1,3 @@
+"""Flagman, real-time payment fraud detection."""
+
+__version__ = "0.1.0"
---
+++ tests/test_sanity.py
@@ -0,0 +1,5 @@
+import flagman
+
+
+def test_package_importable():
+    assert flagman.__version__ == "0.1.0"
